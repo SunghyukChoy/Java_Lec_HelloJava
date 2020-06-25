787bc778e3fb018e461a19cde2d3d99bd069187a
Sunghyuk Choi <likeaglow@gmail.com>
2020-06-25 21:49:19 +0900

    Ch15_sec03_hashset

---
+++ ThisIsJava_Ch15/src/sec03/exam01_hashset/HashSetExample1.java
@@ -0,0 +1,84 @@
+package sec03.exam01_hashset;
+
+import java.util.*;
+
+// String 객체를 중복 없이 저장하는 HashSet
+public class HashSetExample1 {
+
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		// Set 컬렉션 :
+		// 수학의 집합에 비유될 수 있다.
+		// 저장 순서가 유지되지 않는다.
+		// 객체를 중복해서 저장할 수 없고 하나의 null만 저장할 수 있다.
+
+		// HashSet :
+		// 동일 객체 및 동등 객체는 중복 저장하지 않는다.
+		// 동등 객체 판단 방법 :
+		// hashCode() 리턴값 : 같음 -> equals() 리턴값 : 같음 -> 동등 객체
+		// hashCode() 리턴값 : 같음 -> equals() 리턴값 : 다름 -> 다른 객체
+		// hashCode() 리턴값 : 다름 -> 다른 객체
+		/*
+		 * HashSet이 판단하는 동일한 객체란 꼭 같은 인스턴스를 뜻하지는 않는다. HashSet은 객체를 저장하기 전에 먼저 객체의
+		 * hashCode() 메소드를 호출해서 해시코드를 얻어낸다. 그리고 이미 저장되어 있는 객체들의 해시코드와 비교한다. 만약 동일한 해시코드가
+		 * 있다면 다시 equals() 메소드로 두 객체를 비교해서 true가 나오면 동일한 객체로 판단하고 중복 저장을 하지 않는다.
+		 */
+
+		set.add("Java");
+		set.add("JDBC");
+		set.add("Servlet/JSP");
+		set.add("Java"); // "Java"는 한 번만 저장됨
+		set.add("iBATIS");
+
+		int size = set.size(); // 저장된 객체 수 얻기
+		System.out.println("총 객체수: " + size);
+
+		Iterator<String> iterator = set.iterator(); // 반복자 얻기
+		// Set 컬렉션은 인덱스로 객체를 검색해서 가져오는 메소드가 없다.
+		// Set의 타입 파라미터와 Iterater의 타입 파라미터는 같아야 한다. Set 객체의 값들을 가져오기 때문.
+
+		while (iterator.hasNext()) { // 객체 수만큼 루핑 // 가져올 요소가 있느냐
+			String element = iterator.next(); // 한 개의 객체를 가져온다.
+			System.out.println("\t" + element);
+		}
+		
+		set.remove("JDBC"); // 한 개의 객체 삭제
+		set.remove("iBATIS"); // 한 개의 객체 삭제
+
+		System.out.println("총 객체수: " + set.size()); // 저장된 객체 수 얻기
+		
+		iterator = set.iterator();
+		while (iterator.hasNext()) { // 객체 수만큼 루핑
+			String element = iterator.next();
+			System.out.println("\t" + element);
+		}
+
+		System.out.println("총 객체수: " + set.size()); // 저장된 객체 수 얻기
+		
+		for (String elemnet : set) {
+			System.out.println("\t" + elemnet);
+		}
+		
+		while(iterator.hasNext()) {
+			String str = iterator.next();
+			if(str.equals("Java")) {
+				iterator.remove();
+			}			
+		}
+		
+		System.out.println("총 객체수: " + set.size()); // 저장된 객체 수 얻기
+		
+		set.remove("Java");		
+		System.out.println("set.remove 후 총 객체수: " + set.size()); // 저장된 객체 수 얻기
+		iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String str = iterator.next();
+			System.out.println("\t" + str);
+		}
+		
+		set.clear(); // 모든 객체를 제거하고 비움
+		if (set.isEmpty()) {
+			System.out.println("비어 있음");
+		}
+	}
+}
--- ThisIsJava_Ch15/src/sec03/exam01_hashset/Member.java
+++ ThisIsJava_Ch15/src/sec03/exam01_hashset/Member.java
@@ -1,4 +1,6 @@
-package Ex15_3;
+package sec03.exam01_hashset;
+
+import java.util.Objects;
 
 // hashCode()와 equals() 메소드 재정의
 public class Member {
@@ -12,8 +14,8 @@ public class Member {
 	
 	@Override
 	public boolean equals(Object obj) {	// name과 age 값이 같으면 true를 리턴
-		if(obj instanceof Member) {
-			Member member = (Member) obj;
+		if(obj instanceof Member) {	// 매개변수 obj가 Mamber 타입이면
+			Member member = (Member) obj;	// Member로 타입 변환. 
 			return member.name.equals(name) && (member.age == age);
 		} else {
 			return false;
@@ -24,5 +26,6 @@ public class Member {
 	public int hashCode() {	// name과 age 값이 같으면 동일한 hashCode가 리턴
 		return name.hashCode() + age;
 					// String의 hashCode() 이용
+		// return Objects.hash(name, age);
 	}
 }
